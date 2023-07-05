350992118e9e7b79fcfbf3b50dcc32c521f1d955
Geovany Marroquin <pablo.marroquin@pnc.gob.sv>
2023-07-05 07:56:29 -0600

    🎉Init project CountriesApp

--- 03-gifs-app/src/app/shared/shared.module.ts
+++ 03-gifs-app/src/app/shared/shared.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { LazyImageComponent } from './components/lazy-image/lazy-image.component';
 
@@ -10,9 +10,10 @@ import { LazyImageComponent } from './components/lazy-image/lazy-image.component
     SidebarComponent,
     LazyImageComponent
   ],
-  imports: [
-    CommonModule
-  ],
+    imports: [
+        CommonModule,
+        NgOptimizedImage
+    ],
   exports: [
     SidebarComponent,
     LazyImageComponent
