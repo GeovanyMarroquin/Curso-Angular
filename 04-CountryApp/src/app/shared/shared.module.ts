257af163b6b6baa96232c7816c2a17e84e65dfc9
Geovany Marroquin <pablo.marroquin@pnc.gob.sv>
2023-07-07 07:44:40 -0600

    🎨 Basic concepts for Angular routes

--- 04-CountryApp/src/app/shared/shared.module.ts
+++ 04-CountryApp/src/app/shared/shared.module.ts
@@ -1,21 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/home-page/about-page/about-page.component';
-
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
 
 
 @NgModule({
   declarations: [
     AboutPageComponent,
+    ContactPageComponent,
     HomePageComponent,
+    SidebarComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
   ],
   exports: [
     AboutPageComponent,
+    ContactPageComponent,
     HomePageComponent,
+    SidebarComponent,
   ]
 })
 export class SharedModule { }
